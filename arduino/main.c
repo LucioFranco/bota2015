bfa0f9578a8e2fc943d5951ee3d8c0f91215db4d
Lucio Franco <lucio.franco@du.edu>
2015-07-25 19:51:23 -0600

    updated main arudino

--- arduino/main.c
+++ arduino/main.c
@@ -1,8 +1,32 @@
 #include <Wire.h>
 
+#define SLAVE_ADDRESS 0x04
+
 void setup()
 {
-  Wire.begin(4);
-  Wire.onReceive()
-  // continue
+	pinMode(13, OUTPUT);
+	Serial.begin(9600); // start serial for output
+	// initialize i2c as slave
+	Wire.begin(SLAVE_ADDRESS);
+
+	// define callbacks for i2c communication
+	Wire.onReceive(receiveData);
+	Wire.onRequest(sendData);
+
+	Serial.println(“Ready!”);
+}
+//enough time for t
+void loop()
+{
+	delay(100);
+}
+
+// callback for received data
+void receiveData(int byteCount)
+{
+	while(Wire.available())
+	{
+		number = Wire.read();
+
+	}
 }
